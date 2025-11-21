ba33cd8e3b9e9f2f542011ad7dff0d16f1afd42d
Mas Tsabio <prinztsabioprabhudanishwara@gmail.com>
2025-11-21 21:25:08 +0700

    animasi pembuka

---
+++ main.js
@@ -0,0 +1,8 @@
+const hasSim = true;
+if (hasSim) {
+    console.log('kamu boleh mengemudi');
+} else if (hasSim === false) {
+    console.log('kamu nggak boleh mengemudi');
+} else {
+    console.log('kamu ga valid');
+}
